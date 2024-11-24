9ef6d86fd401b06f2693ec8e27bddf95b77f725c
mjmot <mjmot12345@gmail.com>
2024-11-24 11:11:35 +0100

    web

--- lleida_park_api/src/main/java/com/parking/parkingLleida/api/ParkingLotController.java
+++ lleida_park_api/src/main/java/com/parking/parkingLleida/api/ParkingLotController.java
@@ -3,12 +3,10 @@ package com.parking.parkingLleida.api;
 import com.parking.parkingLleida.service.ParkingLotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ParkingLotController {
     @Autowired
     private ParkingLotService parkingLotService;
--- lleida_park_web/script.js
+++ lleida_park_web/script.js
@@ -1,3 +1,4 @@
+
 var map = L.map('map').setView([41.614245, 0.626627], 13);
 
 var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -5,20 +6,50 @@ var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 });
 
 const marker = L.marker([41.610455, 0.6093381]).addTo(map).bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
-let spots = 3;
-let maxSpots = 3;
-let popupContent = `
-    <div>
-    <img src="https://lh5.googleusercontent.com/p/AF1QipO27RUJswSvGPpVkqaudn0OloSZagbTFeUJdK1m=w408-h306-k-no" alt="Image description" width="200">
-        <h2>Marker Title</h2>
-        <p>Ara mateix queden ${spots} de ${maxSpots} places disponibles.</p>
-    </div>
-`;
 
-let marker2 = L.marker([41.6094412,0.6298933]).addTo(map);
+let spots = 0;
+let maxSpots = 0;
+let name = '';
+let marker2 = L.marker([41.6094412, 0.6298933]).addTo(map);
 
+function updatePopup() {
+    let popupContent = `
+        <div>
+        <img src="https://lh5.googleusercontent.com/p/AF1QipO27RUJswSvGPpVkqaudn0OloSZagbTFeUJdK1m=w408-h306-k-no" alt="Image description" width="200">
+            <h2>${name}</h2>
+            <p>Ara mateix queden ${spots} de ${maxSpots} places disponibles.</p>
+        </div>
+    `;
+    marker2.bindPopup(popupContent).openPopup();
+}
 
-marker2.bindPopup(popupContent).openPopup();
-
+$.ajax({
+	url: 'http://192.168.237.224:8080/lot/1',
+	method: 'GET',
+	dataTYpe: 'json',
+	success: function (response) {
+		maxSpots = response.maxSpots;
+		name = response.name;
+		updatePopup();                
+	},
+	error: function (xhr, status, error) {
+		console.error('Error fetching data:', error);
+	}
+});
 
 osm.addTo(map);
+
+
+setInterval(() => {
+    $.ajax({
+        url: 'http://192.168.237.224:8080/lot/1/freeSpots',
+        method: 'GET',
+        success: function (response) {
+            spots = response;
+            updatePopup();                
+        },
+        error: function (xhr, status, error) {
+            console.error('Error fetching data:', error);
+        }
+    });
+}, 1000);
